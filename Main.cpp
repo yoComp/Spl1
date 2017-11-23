e3e2a584c0e3c8a455b596e47b079ba2be9b33ee
yoComp <yonatan0100@gmail.com>
2017-11-23 15:51:40 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,11 @@
-// ... You may not change this file
+#include "Environment.h"
 
-#include "../include/Environment.h"
+// ... You may not change this file
 
 unsigned int verbose = 0;
 
 int main(int , char **) {
     Environment env;
     env.start();
-	return 0;
-}
+    return 0;
+}   
